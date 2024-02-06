474e86594978c6c207c1634fdab6564ff6a18dc6
Lingesh <lingesh@g2techsoft.com>
2024-02-06 16:14:31 +0530

    Fixing Token issues

--- includes/config.php
+++ includes/config.php
@@ -29,3 +29,25 @@ define('DNS_RECORDS_ARRAY', [
     ],
 ]);
  
+
+/** 
+* Provide GitHub details for plugin update 
+*/
+
+$pluginFile =  plugin_basename(dirname(__FILE__)).'/ss-cloudflare.php';
+
+// DEFINE plugin SLUG
+define('CloudFlareSLUG', $pluginFile);
+
+// DEFINE GITHUB user name
+define('CloudFlareGITHUBUSERNAME', 'lingeshdeepakg2');
+
+// DEFINE GITHUB project repo name
+define('CloudFlareGITHUBPROJECTREPO', 'ss-cloudflare');
+
+// DEFINE plugin GITHUB project branch name
+define('CloudFlareGITHUBBRANCH','main');
+
+// DEFINE AccessToken created in GITHUB
+// If the Git repo is private provide the Access Token, if the Git repo is public leave it empty
+define('CloudFlareACCESSTOKEN',"");
